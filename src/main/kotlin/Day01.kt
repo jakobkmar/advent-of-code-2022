9544fd72e053a77958a8a5028d305904ba850fda
Jakob K <dev@jakobk.de>
2022-12-01 13:52:04 +0100

    Only parse day 01 input once

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -1,13 +1,14 @@
 fun main() = day(1) {
+
+    val input = inputString.split("\n\n")
+        .map { g -> g.lines().sumOf { it.toInt() } }
+
     part1 {
-        inputString.split("\n\n")
-            .maxOfOrNull { g -> g.lines().sumOf { it.toInt() } }
+        input.max()
     }
 
     part2 {
-        inputString.split("\n\n")
-            .map { g -> g.lines().sumOf { it.toInt() } }
-            .sortedDescending().take(3).sum()
+        input.sortedDescending().take(3).sum()
     }
 
     expectPart1 = 24000
