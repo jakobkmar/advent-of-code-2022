5a05c4997dafd4c2d4f08508556942d081c5d6ea
Jakob K <dev@jakobk.de>
2022-12-02 07:35:42 +0100

    Reorder values and functions

--- src/main/kotlin/Day02.kt
+++ src/main/kotlin/Day02.kt
@@ -1,14 +1,14 @@
 fun main() = day(2) {
 
     val abc = "ABC".toList()
+    val input = inputLines.map { it.split(' ').map(String::first) }
 
-    fun Char.shift(n: Int) = abc[(abc.indexOf(this) + n) % 3]
+    fun Char.shift(n: Int) =
+        abc[(abc.indexOf(this) + n) % 3]
 
     fun score(other: Char, me: Char) =
         when (me) { other -> 3; other.shift(1) -> 6; else -> 0 } +
             abc.indexOf(me) + 1
-
-    val input = inputLines.map { it.split(' ').map(String::first) }
 
     part1 {
         input.sumOf { (other, me) -> score(other, abc["XYZ".indexOf(me)]) }
