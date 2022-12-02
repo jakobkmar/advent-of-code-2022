8c0b902edef88e48e9a020db1f087398eb210ddf
Jakob K <dev@jakobk.de>
2022-12-02 07:19:18 +0100

    Small day 02 overhaul

--- src/main/kotlin/Day02.kt
+++ src/main/kotlin/Day02.kt
@@ -4,21 +4,19 @@ fun main() = day(2) {
 
     fun Char.shift(n: Int) = options[(options.indexOf(this) + n) % 3]
 
-    fun Char.xyzToAbc() = when (this) { 'X' -> 'A'; 'Y' -> 'B'; 'Z' -> 'C'; else -> error(Unit) }
-
     fun score(other: Char, me: Char) =
         when (me) { other -> 3; other.shift(1) -> 6; else -> 0 } +
-            when (me) { 'A' -> 1; 'B' -> 2; 'C' -> 3; else -> error(Unit) }
+            options.indexOf(me) + 1
 
     val input = inputLines.map { l -> l.split(' ').map { it[0] } }
 
     part1 {
-        input.sumOf { (other, me) -> score(other, me.xyzToAbc()) }
+        input.sumOf { (other, me) -> score(other, options["XYZ".indexOf(me)]) }
     }
 
     part2 {
         input.sumOf { (other, iMust) ->
-            val shift = when (iMust) { 'X' -> 2; 'Y' -> 0; 'Z' -> 1; else -> error(Unit) }
+            val shift = when (iMust) { 'X' -> 2; 'Z' -> 1; else -> 0 }
             score(other, other.shift(shift))
         }
     }
