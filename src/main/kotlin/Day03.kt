511fa950b503da531d56cd8479c9f2fd53bbf9eb
Jakob K <dev@jakobk.de>
2022-12-03 06:18:19 +0100

    Initial solution for day 03

---
+++ src/main/kotlin/Day03.kt
@@ -0,0 +1,17 @@
+fun main() = day(3) {
+
+    val priorities = (('a'..'z') + ('A'..'Z')).mapIndexed { i, c -> c to i + 1 }.toMap()
+
+    part1 {
+        inputLines.map(String::toList)
+            .sumOf { priorities[(it.take(it.size / 2) intersect it.takeLast(it.size / 2).toSet()).single()]!! }
+    }
+
+    part2 {
+        inputLines.map(String::toSet).chunked(3)
+            .sumOf { priorities[(it[0] intersect it[1] intersect it[2]).single()]!! }
+    }
+
+    expectPart1 = 157
+    expectPart2 = 70
+}
