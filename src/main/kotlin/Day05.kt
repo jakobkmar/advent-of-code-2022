a08adc6570906656ccd28de2e6fa8ea88b30889e
Jakob K <dev@jakobk.de>
2022-12-05 19:34:17 +0100

    Shorten day 5 with new scope system

--- src/main/kotlin/Day05.kt
+++ src/main/kotlin/Day05.kt
@@ -2,41 +2,37 @@ typealias Stacks = MutableMap<Int, ArrayDeque<Char>>
 
 fun main() = day(5) {
 
-    fun solve(crane: (stacks: Stacks, Int, Int, Int) -> Unit): String {
-        val (firstPart, secondPart) = inputString.split("\n\n").map { it.lines() }
+    val (firstPart, secondPart) = inputString.split("\n\n").map { it.lines() }
 
-        val stacks: Stacks = sortedMapOf()
-        firstPart.take(firstPart.size - 1).forEach { l ->
-            Regex("""(\[.]| {3})""").findAll(l.filterIndexed { index, _ -> ((index + 1) % 4) != 0 })
-                .forEachIndexed { index, result ->
-                    result.value.singleOrNull(Char::isLetter)
-                        ?.let { stacks.getOrPut(index + 1) { ArrayDeque() }.addLast(it) }
-                }
-        }
+    val stacks: Stacks = sortedMapOf()
+    firstPart.take(firstPart.size - 1).forEach { l ->
+        Regex("""(\[.]| {3})""").findAll(l.filterIndexed { index, _ -> ((index + 1) % 4) != 0 })
+            .forEachIndexed { index, result ->
+                result.value.singleOrNull(Char::isLetter)
+                    ?.let { stacks.getOrPut(index + 1) { ArrayDeque() }.addLast(it) }
+            }
+    }
 
+    fun solve(crane: (Int, Int, Int) -> Unit): String {
         secondPart.forEach { l ->
             val (amount, from, to) = l.removePrefix("move ").split(" from ", " to ")
                 .map { it.toInt() }
-            crane(stacks, amount, from, to)
+            crane(amount, from, to)
         }
 
         return stacks.map { it.value.first() }.joinToString("")
     }
 
     part1 {
-        solve { stacks, amount, from, to ->
-            repeat(amount) {
-                stacks[to]!!.addFirst(stacks[from]!!.removeFirst())
-            }
+        solve { amount, from, to ->
+            repeat(amount) { stacks[to]!!.addFirst(stacks[from]!!.removeFirst()) }
         }
     }
 
     part2 {
-        solve { stacks, amount, from, to ->
-            val moving = (1..amount).map { stacks[from]!!.removeFirst() }
-            moving.reversed().forEach {
-                stacks[to]!!.addFirst(it)
-            }
+        solve { amount, from, to ->
+            (1..amount).map { stacks[from]!!.removeFirst() }.reversed()
+                .forEach { stacks[to]!!.addFirst(it) }
         }
     }
 
