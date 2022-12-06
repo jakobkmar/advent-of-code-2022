22cb4417f148d693340c48702c9103a5a2654a17
Jakob K <dev@jakobk.de>
2022-12-06 13:43:21 +0100

    Further shorten day 6

--- src/main/kotlin/Day06.kt
+++ src/main/kotlin/Day06.kt
@@ -1,17 +1,10 @@
 fun main() = day(6) {
 
-    fun findPacket(size: Int) =
-        inputString.toList().windowed(size).withIndex()
-            .first { it.value.distinct().size == it.value.size }
-            .index + size
+    fun findPacket(size: Int) = inputString.windowed(size)
+        .indexOfFirst { it.toSet().size == size } + size
 
-    part1 {
-        findPacket(4)
-    }
-
-    part2 {
-        findPacket(14)
-    }
+    part1 { findPacket(4) }
+    part2 { findPacket(14) }
 
     expectPart1 = 7
     expectPart2 = 19
