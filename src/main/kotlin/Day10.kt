53e98a3270b66e4b2e7bf7967167909fae20de1d
Jakob K <dev@jakobk.de>
2022-12-10 07:16:55 +0100

    Upload solution for day 10

---
+++ src/main/kotlin/Day10.kt
@@ -0,0 +1,24 @@
+fun main() = day(10) {
+
+    val xValues = inputLines.flatMap {
+        if (it == "noop") listOf(0) else listOf(0, it.split(' ')[1].toInt())
+    }.scan(1, Int::plus)
+
+    part1 {
+        (20..220 step 40).sumOf { it * xValues[it - 1] }
+    }
+
+    part2 {
+        val screen = xValues
+            .chunked(40)
+            .joinToString("\n") {
+                it.withIndex().joinToString("") { (index, xValue) ->
+                    if (xValue in (index - 1)..(index + 1)) "█" else " "
+                }
+            }
+        println(screen)
+    }
+
+    expectPart1 = 13140
+    expectPart2 = Unit
+}
