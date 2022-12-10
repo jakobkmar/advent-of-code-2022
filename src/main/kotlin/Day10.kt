f678c9424b3706b978978a7842318d3761f9fc41
Jakob K <dev@jakobk.de>
2022-12-10 07:31:16 +0100

    Increase day 10 parsing logic readability

--- src/main/kotlin/Day10.kt
+++ src/main/kotlin/Day10.kt
@@ -1,8 +1,8 @@
 fun main() = day(10) {
 
-    val xValues = inputLines.flatMap {
-        if (it == "noop") listOf(0) else listOf(0, it.split(' ')[1].toInt())
-    }.scan(1, Int::plus)
+    val xValues = inputLines
+        .flatMap { if (it == "noop") listOf(0) else listOf(0, it.split(' ')[1].toInt()) }
+        .scan(1, Int::plus)
 
     part1 {
         (20..220 step 40).sumOf { it * xValues[it - 1] }
